d4d3d584e3b9195d011611e9d32bf8f5e9826b65
kasumil <ridou99@gmail.com>
2021-11-12 22:14:46 +0900

    body parser and req,res 위치 변경

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extension: false}));
 
 dotenv.config();
 
-app.listen('5000', (res, req, next) => {
+app.listen('5000', (req, res, next) => {
     console.log('5000번 포트에서 시작되었습니다.');
 });
 
